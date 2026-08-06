7054a9a3bf15f87e68f46a112311e30e684f2b9b
Raghav Gupta <raghavgupta805288@gmail.com>
2026-08-06 21:07:30 +0530

    Update authService.js

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/auth";
+const API_URL = "https://ai-energy-optimizer-backend.onrender.com/api/auth";
 
 // Register
 export const register = async (userData) => {
